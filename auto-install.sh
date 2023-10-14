05700f9524a7ad040f5bd1ebf2f78c14e485a538
l0py2 <l0py2.git@gmail.com>
2023-10-14 01:33:46 +0100

    Added several mods

---
+++ auto-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+printf '' > modlist && cat README.md | while read -r line; do echo $line | \
+	grep -o 'http[a-zA-Z0-9/:\.-]*' >> modlist; done
+
+cat modlist | while read -r line; do packwiz cf add -y $line; done;
