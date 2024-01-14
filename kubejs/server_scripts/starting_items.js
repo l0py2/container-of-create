37e325a14315bd708891cfc5986e3be4a148cfb7
l0py2 <l0py2.git@gmail.com>
2024-01-14 00:32:01 +0000

    Fixed starting items

--- kubejs/server_scripts/starting_items.js
+++ kubejs/server_scripts/starting_items.js
@@ -4,15 +4,14 @@ PlayerEvents.loggedIn(event => {
 	if(!event.player.stages.has(starting_stage)) {
 		event.player.stages.add(starting_stage);
 
-		const items = [
-			'create_sa:copper_pickaxe',
-			'comforts:sleeping_bag_white',
-			'sophisticatedbackpacks:backpack',
-			'create_sa:copper_chestplate',
-			'create_sa:copper_leggings',
-			'minecraft:leather_boots'
-		];
-
-		items.forEach(item => event.player.give(item));
+		event.player.give('create_sa:copper_pickaxe');
+		event.player.give('comforts:sleeping_bag_white');
+		event.player.give('sophisticatedbackpacks:backpack');
+		event.player.give('create_sa:copper_chestplate');
+		event.player.give('create_sa:copper_leggings');
+		event.player.give('minecraft:leather_boots');
+		event.player.give('16x minecraft:apple');
+		event.player.give('4x farmersdelight:egg_sandwich');
+		event.player.give('4x farmersdelight:chicken_sandwich');
 	}
 });
