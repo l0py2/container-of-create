ceb0926b20df2a1003a5bbc6c11fa6f1ac2a09f8
l0py2 <l0py2.git@gmail.com>
2024-07-09 17:55:59 +0100

    Removed Create: The Factory Must Grow

--- kubejs/server_scripts/compat/oil.js
+++ kubejs/server_scripts/compat/oil.js
@@ -1,5 +1,4 @@
 ServerEvents.tags('item', event => {
-	event.add('ad_astra:oil', 'tfmg:crude_oil');
 	event.add('ad_astra:oil', 'thermal:crude_oil');
 	event.add('forge:crude_oil', 'thermal:crude_oil');
 });
